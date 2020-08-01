0c736f06a579078e0d98da57a1107bb7b8fe7baf
M Weltin <mweltin@gmail.com>
2020-08-01 15:08:32 -0700

    add confusion matrix plot  generator

--- simple_sequential_model.py
+++ simple_sequential_model.py
@@ -6,6 +6,9 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import categorical_crossentropy
 import numpy as np
 import data_prep
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
@@ -20,7 +23,49 @@ model = Sequential([
 
 model.summary()
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(x=data_prep.scaled_train_samples, y=data_prep.train_labels, validation_split=0.1, batch_size=10, epochs=30, shuffle=True, verbose=2)
+model.fit(x=data_prep.scaled_train_samples, y=data_prep.train_labels, validation_split=0.1, batch_size=10, epochs=30,
+          shuffle=True, verbose=2)
 predictions = model.predict(x=data_prep.scaled_test_samples, batch_size=10, verbose=0)
 rounded_predictions = np.argmax(predictions, axis=-1)
 
+cm = confusion_matrix(y_true=data_prep.test_labels, y_pred=rounded_predictions)
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    #plt.show()
+    plt.savefig("confusion.png")
+
+
+cm_plot_labels = ['no_side_effects', 'had_side_effects']
+plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title='Confusion Matrix')
