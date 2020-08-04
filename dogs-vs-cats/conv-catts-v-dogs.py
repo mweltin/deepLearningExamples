aaba55f0bdffd6c50be591be49fd58a946b2cae3
M Weltin <mweltin@gmail.com>
2020-08-03 18:16:44 -0700

    rename files full network trains

--- dogs-vs-cats/conv-catts-v-dogs.py
+++ dogs-vs-cats/conv-catts-v-dogs.py
@@ -40,17 +40,17 @@ if os.path.isdir('train/dog') is False:
     os.makedirs('test/dog')
     os.makedirs('test/cat')
 
-for c in random.sample(glob.glob('../../train/cat*'), 500):
+for c in random.sample(glob.glob('../../train/cat*'), 1000):
     shutil.copy(c, 'train/cat')
-for c in random.sample(glob.glob('../../train/dog*'), 500):
+for c in random.sample(glob.glob('../../train/dog*'), 1000):
     shutil.copy(c, 'train/dog')
-for c in random.sample(glob.glob('../../train/cat*'), 100):
+for c in random.sample(glob.glob('../../train/cat*'), 200):
     shutil.copy(c, 'valid/cat')
-for c in random.sample(glob.glob('../../train/dog*'), 100):
+for c in random.sample(glob.glob('../../train/dog*'), 200):
     shutil.copy(c, 'valid/dog')
-for c in random.sample(glob.glob('../../train/cat*'), 50):
+for c in random.sample(glob.glob('../../train/cat*'), 100):
     shutil.copy(c, 'test/cat')
-for c in random.sample(glob.glob('../../train/dog*'), 50):
+for c in random.sample(glob.glob('../../train/dog*'), 100):
     shutil.copy(c, 'test/dog')
 
 os.chdir('../../')
@@ -66,10 +66,11 @@ valid_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.
 test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
     .flow_from_directory(directory=test_path, target_size=(224, 224), classes=['cat', 'dog'], batch_size=10)
 
-assert train_batches.n == 1000
-assert valid_batches.n == 200
-assert test_batches.n == 100
+assert train_batches.n == 2000
+assert valid_batches.n == 400
+assert test_batches.n == 200
 assert train_batches.num_classes == valid_batches.num_classes == test_batches.num_classes == 2
+"""
 imgs, labels = train_batches.next()
 def plotImages(images_arr):
     fig, axes = plt.subplots(1, 10, figsize=(20, 20))
@@ -84,3 +85,16 @@ def plotImages(images_arr):
 
 plotImages(imgs)
 print(labels)
+"""
+
+model = Sequential([
+    Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(224, 224, 3)),
+    MaxPool2D(pool_size=(2, 2), strides=2),
+    Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'),
+    MaxPool2D(pool_size=(2, 2), strides=2),
+    Flatten(),
+    Dense(units=2, activation='softmax')
+])
+model.summary()
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(x=train_batches, validation_data=valid_batches, epochs=10, verbose=2)
