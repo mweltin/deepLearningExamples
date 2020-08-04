374f6a64cc565e472e52d14ef152cd1d4096b108
M Weltin <mweltin@gmail.com>
2020-08-03 21:38:29 -0700

    refactor plots are not in their own png

--- dogs-vs-cats/conv-catts-v-dogs.py
+++ dogs-vs-cats/conv-catts-v-dogs.py
@@ -14,6 +14,7 @@ import random
 import glob
 import matplotlib.pyplot as plt
 import warnings
+import dlplots
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 # %matplotlib inline
@@ -40,17 +41,20 @@ if os.path.isdir('train/dog') is False:
     os.makedirs('test/dog')
     os.makedirs('test/cat')
 
-for c in random.sample(glob.glob('../../train/cat*'), 1000):
+train_size = 2000
+valid_size = 100
+test_size = 100
+for c in random.sample(glob.glob('../../train/cat*'), train_size):
     shutil.copy(c, 'train/cat')
-for c in random.sample(glob.glob('../../train/dog*'), 1000):
+for c in random.sample(glob.glob('../../train/dog*'), train_size):
     shutil.copy(c, 'train/dog')
-for c in random.sample(glob.glob('../../train/cat*'), 200):
+for c in random.sample(glob.glob('../../train/cat*'), valid_size):
     shutil.copy(c, 'valid/cat')
-for c in random.sample(glob.glob('../../train/dog*'), 200):
+for c in random.sample(glob.glob('../../train/dog*'), valid_size):
     shutil.copy(c, 'valid/dog')
-for c in random.sample(glob.glob('../../train/cat*'), 100):
+for c in random.sample(glob.glob('../../train/cat*'), test_size):
     shutil.copy(c, 'test/cat')
-for c in random.sample(glob.glob('../../train/dog*'), 100):
+for c in random.sample(glob.glob('../../train/dog*'), test_size):
     shutil.copy(c, 'test/dog')
 
 os.chdir('../../')
@@ -66,24 +70,15 @@ valid_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.
 test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
     .flow_from_directory(directory=test_path, target_size=(224, 224), classes=['cat', 'dog'], batch_size=10)
 
-assert train_batches.n == 2000
-assert valid_batches.n == 400
-assert test_batches.n == 200
+assert train_batches.n == train_size * 2
+assert valid_batches.n == valid_size * 2
+assert test_batches.n == test_size * 2
 assert train_batches.num_classes == valid_batches.num_classes == test_batches.num_classes == 2
+
+
 """
 imgs, labels = train_batches.next()
-def plotImages(images_arr):
-    fig, axes = plt.subplots(1, 10, figsize=(20, 20))
-    axes = axes.flatten()
-    for img, ax in zip(images_arr, axes):
-        ax.imshow(img)
-        ax.axis('off')
-    plt.tight_layout()
-    # plt.show()
-    plt.savefig("dogs-v-cat.png")
-
-
-plotImages(imgs)
+dlplots.plotImages(imgs)
 print(labels)
 """
 
@@ -97,4 +92,16 @@ model = Sequential([
 ])
 model.summary()
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(x=train_batches, validation_data=valid_batches, epochs=10, verbose=2)
+model.fit(x=train_batches, validation_data=valid_batches, epochs=2, verbose=2)
+
+test_images, test_labels = test_batches.next()
+dlplots.plotImages(test_images)
+print(test_labels)
+
+test_batches.classes
+predictions = model.predict(x=test_batches, verbose=0)
+np.round(predictions)
+cm = confusion_matrix(y_true=test_batches.classes, y_pred=np.argmax(predictions, axis=1))
+test_batches.class_indices
+cm_plot_labels = ['cats', 'dogs']
+dlplots.plot_confusion_matrix(cm, classes=cm_plot_labels, title='confusion in a jar')
